7724b1c73c2e94413a6420288758d3785a299bdc
Aleksandra Kobusińska <kobusinska.ola@gmail.com>
2017-08-25 09:57:50 +0200

    created Spring Boot project, added SQL commands files

---
+++ src/main/java/javaproject/payItForward/PayItForwardApplication.java
@@ -0,0 +1,12 @@
+package javaproject.payItForward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayItForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayItForwardApplication.class, args);
+	}
+}
